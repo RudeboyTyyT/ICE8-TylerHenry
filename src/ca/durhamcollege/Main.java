f54c9b5018fd2efc86cea2833664e9acde1ccc3d
Tyler Henry <tyler.henry2@dcmail.ca>
2021-11-11 11:54:08 -0500

    added multi-dimensional array example

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -24,7 +24,7 @@ public class Main
         float age = 0.0f;
 
         boolean isValidInput = false; // sentinel variable
-        while(!isValidInput)
+        do
         {
             System.out.print("Please enter your age: ");
             try
@@ -49,11 +49,37 @@ public class Main
             }
 
         }
-
-
+        while(!isValidInput);
         System.out.println();
         //Output
         System.out.printf("You Entered: %s \n", name);
         System.out.printf("You Entered: %.2f \n", age);
+
+
+        //multi-dimensional array example
+        int rows = 8;
+        int cols = 8;
+        float[][] board = new float[rows][cols];
+
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < cols; y++)
+            {
+                board[x][y] = (float)(Math.random() + 100.0f) + 1.0f;
+            }
+        }
+        System.out.println("Showing 10 Random Values: ");
+        for (int i = 0; i < 10; i++)
+        {
+            var randRows = (int)(Math.random() *rows);
+            var randCols = (int)(Math.random() *cols);
+            System.out.print("For row: " + randRows + " cols: " + randCols + " ");
+            System.out.printf("The random number is %.2f \n", board[randRows][randCols]);
+        }
+
+
+
+
+
     }
 }
